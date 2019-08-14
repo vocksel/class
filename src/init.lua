56e0adc219cfac89589a20beaf85ec785feefec8
David Minnerly <voxeldavid@gmail.com>
2019-08-14 13:40:49 -0400

    Remove 'Super' property

--- src/init.lua
+++ src/init.lua
@@ -14,19 +14,6 @@ local function copyMetaMethods(from, to)
 end
 
 local function subclass(NewClass, SuperClass)
-	-- You should always reference the class' superclass by setting a `super`
-	--  variable to the second return value of `class()`.
-	--
-	-- This is kept for compatibility reasons, and there are certain scenarios
-	-- where using the above won't be advantageous. For example, if you're
-	-- defining a lot of classes in a single file, you won't be able to define a
-	-- `super` variable without it conflicting.
-	--
-	-- Be careful when using this property. If you call a superclass' method that
-	-- references `self.Super`, it will cause a stack overflow as `self` is the
-	-- subclass.
-	NewClass.Super = SuperClass
-
 	setmetatable(NewClass, SuperClass)
 	copyMetaMethods(SuperClass, NewClass)
 end
